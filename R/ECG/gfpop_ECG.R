1e785c2f5739c11cba86f29913f116be257597d9
Amric Trudel <amrictrudel.42@gmail.com>
2023-03-15 16:47:26 +0100

    refactor graph model display as a function

--- R/ECG/gfpop_ECG.R
+++ R/ECG/gfpop_ECG.R
@@ -22,84 +22,96 @@ heartbeatGraph <- gfpop::graph(
 )
 
 # Fit graph to signal
-fit <- gfpop(data = ECG$data$millivolts, mygraph = heartbeatGraph, type = "mean")
+fitted_model <- gfpop(data = ECG$data$millivolts, mygraph = heartbeatGraph, type = "mean")
 
 
-# Build a table that describes segments
-#------------------------------------------------------------------------------------------------
-end.i <- fit$changepoints
-start.i <- c(1, end.i[-length(end.i)]+1)
+plot_qrs_modelled_signal <- function(
+  fitted_model,
+  dataset,
+  sampling_frequency
+){
+  ## Build a table that describes segments
+  ##
+  end.i <- fitted_model$changepoints
+  start.i <- c(1, end.i[-length(end.i)]+1)
 
-# Create initial table with 4 columns
-segments.dt <- with(ECG$data, data.table(
-  timeStart=time[start.i],
-  timeEnd=time[end.i],
-  state=as.numeric(fit$states),
-  mean=fit$parameters
-))
+  # Create initial table with 4 columns
+  segments.dt <- with(dataset, data.table(
+    timeStart=time[start.i],
+    timeEnd=time[end.i],
+    state=as.numeric(fitted_model$states),
+    mean=fitted_model$parameters
+  ))
 
-# Assign a letter to each state in a separate column
-segments.dt[, letter := c("beforeQ", "Q", "R", "S", "S1", "S2", "peak", "afterPeak", "foo")[state+1] ]
+  # Assign a letter to each state in a separate column
+  segments.dt[, letter := c("beforeQ", "Q", "R", "S", "S1", "S2", "peak", "afterPeak", "foo")[state+1] ]
 
-# Create a new table with two rwos per segment. For a given segment:
-# The "time" column has the beginning of the segment -0,5 on the first row
-# The "time" column gas the end of the segment + 0.5 on the first row
-# The "mean" column has a copy of the mean on both rows.
-mean.dt <- segments.dt[, data.table(
-  time=as.numeric(rbind(timeStart-0.5, timeEnd+0.5)),
-  mean=as.numeric(rbind(mean, mean)) # rbind concatenates rows together
-)]
-
-
-model.dt = segments.dt[, data.table(
-    time=ifelse(letter=='Q', timeEnd, (timeStart+timeEnd)/2),
-    millivolts=mean,
-    letter
+  # Create a new table with two rwos per segment. For a given segment:
+  # The "time" column has the beginning of the segment -0,5 on the first row
+  # The "time" column gas the end of the segment + 0.5 on the first row
+  # The "mean" column has a copy of the mean on both rows.
+  mean.dt <- segments.dt[, data.table(
+    time=as.numeric(rbind(timeStart-0.5, timeEnd+0.5)),
+    mean=as.numeric(rbind(mean, mean)) # rbind concatenates rows together
   )]
 
-samples.per.second <- 250
-truth.dt <- segments.dt[letter=="R", list(time=(timeStart+timeEnd)/2)]
+
+  model.dt = segments.dt[, data.table(
+      time=ifelse(letter=='Q', timeEnd, (timeStart+timeEnd)/2),
+      millivolts=mean,
+      letter
+    )]
+
+  truth.dt <- segments.dt[letter=="R", list(time=(timeStart+timeEnd)/2)]
 
 
-# Plot
-#------------------------------------------------------------------------------------------------
-gg <- ggplot()+
-  geom_vline(aes(
-    xintercept=time/samples.per.second),
-    color="red",
-    data=truth.dt)+
-  # Plot the true R spikes in red
-  geom_text(aes(
-    x, y, hjust=hjust, label="True R"),
-    color="red",
-    size=3,
-    data=data.table(
-      x=208.5, y=6500, hjust=1, label="True R"))+
-  theme_bw()+
-  theme(panel.spacing=grid::unit(0, "lines"))+
-  # Plot the ECG signal in grey
-  geom_line(aes(
-    time/samples.per.second, millivolts),
-    color="grey50",
-    data=ECG$data)+
-  # Plot the mean-based model in blue
-  geom_line(aes(
-    time/samples.per.second, mean),
-    data=mean.dt,
-    color="blue")+
-  # Plot the state labels on top of the model plot in blue
-  geom_label(aes(
-    time/samples.per.second, millivolts,
-    label=letter),
-    color="blue",
-    size=3,
-    label.padding=grid::unit(0.1, "lines"),
-    alpha=0.6,
-    data=model.dt)+
-  coord_cartesian(xlim=c(52000, 52900)/samples.per.second, expand=FALSE)+
-  xlab("Time (seconds)")+
-  ylab("Electrocardiogram activity (mV)")
+  # Plot
+  #------------------------------------------------------------------------------------------------
+  ggplot()+
+    # Plot the true R spikes in red
+    geom_vline(aes(
+      xintercept=time/sampling_frequency),
+      color="red",
+      data=truth.dt)+
+    geom_text(aes(
+      x, y, hjust=hjust, label="True R"),
+      color="red",
+      size=3,
+      data=data.table(
+        x=208.5, y=6500, hjust=1, label="True R"))+
+    theme_bw()+
+    theme(panel.spacing=grid::unit(0, "lines"))+
+    # Plot the ECG signal in grey
+    geom_line(aes(
+      time/sampling_frequency, millivolts),
+      color="grey50",
+      data=ECG$data)+
+    # Plot the mean-based model in blue
+    geom_line(aes(
+      time/sampling_frequency, mean),
+      data=mean.dt,
+      color="blue")+
+    # Plot the state labels on top of the model plot in blue
+    geom_label(aes(
+      time/sampling_frequency, millivolts,
+      label=letter),
+      color="blue",
+      size=3,
+      label.padding=grid::unit(0.1, "lines"),
+      alpha=0.6,
+      data=model.dt)+
+    coord_cartesian(xlim=c(52000, 52900)/sampling_frequency, expand=FALSE)+
+    xlab("Time (seconds)")+
+    ylab("Electrocardiogram activity (mV)")
+}
 
+gg <- plot_qrs_modelled_signal(
+  fitted_model = fitted_model,
+  dataset = ECG$data,
+  sampling_frequency = 250
+)
+
+show(gg)
 pdf("R/ECG/gfpop_ECG.pdf")
 print(gg)
 dev.off()
