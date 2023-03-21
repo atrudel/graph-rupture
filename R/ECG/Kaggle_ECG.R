4f711233150f73b7a2e0554abf0fc9f29a5bec07
Vincent Garot <vincent.garot@ens-lyon.fr>
2023-03-21 16:56:39 +0100

    data updated for ECG5days

--- R/ECG/Kaggle_ECG.R
+++ R/ECG/Kaggle_ECG.R
@@ -26,11 +26,11 @@ signal <- extract_signal(idx, heartbeats)
 signal <- signal[-c(1,2),]
 
 # Create the graph
-scaling_factor <- 1 # Scale the gaps by this factor to transfer the graph from one dataset to another
+scaling_factor <- 0.2 # Scale the gaps by this factor to transfer the graph from one dataset to another
 qrs_graph <- gfpop::graph(
   gfpop::Edge(0, 1, type = "down", penalty = 8e7, gap=0),
   gfpop::Edge(1, 2, type = "up", penalty = 0, gap=2*scaling_factor),
-  gfpop::Edge(2, 3, type = "down", penalty = 0, gap=5*scaling_factor),
+  gfpop::Edge(2, 3, type = "down", penalty = 0, gap=3*scaling_factor),
   gfpop::Edge(3, 4, type = "up", penalty = 0, gap=2*scaling_factor),
   gfpop::Edge(4, 5, type = "up", penalty = 0, gap=1*scaling_factor),
   gfpop::Edge(5, 6, type = "up", penalty = 0, gap=0),
